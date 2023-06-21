e3cad40c1cf5bb173f6d7462caf1e14d67d346dd
EternalModz <86510009+EternalModz@users.noreply.github.com>
2023-06-20 19:07:37 -0700

    Many Fixes
    Removed useless files and code, updated credits, added some notes and cleaned up some things.

--- Source SPRX/Modcraft_Promo/PS3_Debug/SPRX to HEN.bat
+++
@@ -1,2 +0,0 @@
-cd C:\Users\Misaki\ps3tools\ps3tools\tools\
-PKG_ContentID.exe --resign "C:\Users\Misaki\Desktop\Source\Modcraft_Promo\PS3_Debug\EBOOT.BIN"
---
+++ Stuff/nativeCaller.h
@@ -0,0 +1,488 @@
+#include "lib.h";
+
+char* SomeStrings[] = { "aXprogressbar", "aTitle", "aGmicon", "aMgLogo", "aConfirm", "aTimer_0", "aTitle_0", "aDeathMessage", "aExtraMessage","aXrespond", "aExitgame", "aGameoptions", "aGameplayers", "aTitle_1"};
+
+char* StringsFount2[] = { "aPanelaward", "aStat1label", "aStat2label", "aStat3label", "aStat3label", "aStat4label", "aStat5label", "aKillbar", "aKilledbylabel", "aGamertaglabel", "aPanelscoreboard", "aPlayertitle", "aScoreboard" };
+
+char* StringsFount3[] = {"aXhostoptionslis"};
+
+char* StringsFount4[] = { "aXPlayeroptions", "aGamertag" };
+
+char* StringsFount5[] = { "aAcceptbutton", "aEULAtext" };
+
+char* StringsFount6[] = { "MainPanel", "DecriptionPanel", "DecriptionTextTop", "CraftingHSlots", "SpinnerTitle", "Slot2_1", "Slot2_2", "Slot_3_1", "Slot_3_2", "Slot_3_3", "SlotSelector3" "Crafting3VSlot1", "Crafting3VSlot2", "Crafting2VSlot3", "SlotSelector2", "Crafting2VSlot1", "Crafting2VSlot2", "CraftingSelector", "Crafting1VSlot1"
+"PreviewLabel", "IngredientsLayout", "IngredientsLayout_Small", "CraftingOutput", "Ingredient1", "Ingredient2", "Ingredient3", "Ingredient4", "Ingredient5", "IngredientDesc1", "IngredientDesc2", "IngredientDesc3", "IngredientDesc4", "IngredientDesc5", "PreviewSlot", "IngredientsLabel", "DecriptionText", "Inventory", "HotBar", "GroupName"
+"ItemName", "InventoryLabel"};
+char* _StringsFount6[] = { "" };
+
+char* StringsFount7[] = { "Result", "CraftingList", "TitleLabel" };
+char* _StringsFount7[] = { "aResult", "aCraftinglist", "aTitlelabel" };
+
+char* StringsFount8[] = { "ActivatorSlot", "ActivatorList", "Primary_Slot_01", "Primary_Slot_02", "Primary_Slot_03", "Primary_Slot_04", "Primary_Slot_05", "Secondary_Slot_01", "Secondary_Slot_02", "ConfirmButtom", "PrimaryPowerLabel", "SecondaryPowerLabel" };
+char* _StringsFount8[] = { "" };
+
+char* StringsFount9[] = { "Trap", "dispenserLabel" };
+char* _StringsFount9[] = { "aTrap", "aDispenserlabel" };
+
+char* StringsFount10[] = { "Ingredient", "Ingredient2", "Result", "AnvilText", "AnvilInput" };
+char* _StringsFount10[] = { "aIngredient", "aIngredient2_0", "aResult_0", "aAnviltext", "aAnvilinput" };
+
+char* StringsFount11[] = { "TouchPanel_0", "TouchPanel_1", "TouchPanel_2", "TouchPanel_3", "TouchPanel_4", "TouchPanel_5", "TouchPanel_6", "TouchPanel_7", "TouchPanel_Slider", "containerlist" };
+char* _StringsFount11[] = { "aTouchpanel0", "aTouchpanel1", "aTouchpanel2", "aTouchpanel3", "aTouchPanel4", "aTouchpanel5", "aTouchpanel6", "aTouchpanel7", "aTouchpanelSlider", "aContainerlist" };
+
+
+char* StringsFount12[] = { "Ingredient", "Fuel", "Result", "Furance_text", "Ingredient_Label", "Fuel_Label", "FurnaceFire", "FurnaceArrow" };
+char* _StringsFount12[] = { "" };
+
+char* StringsFount13[] = { "ingredient", "lapis", "Button1", "Button2", "Button3", "enchantLabel", "iggy_EnchantmentBook" };
+
+char* StringsFount14[] = { "Trap", "dispenserLabel" };
+char* _StringFount14[] = { "aTrap_0", "aDispenserlabel_0" };
+
+char* StringsFount15[] = { "containerList", "chestLabel" };
+char* _StringsFount15[] = { "aContainerlist_0", "aChestlabel" };
+
+char* StringsFount16[] = { "Bottle1", "Bottle2", "Bottle3", "Ingredient", "BrewingPlate", "BrewingStandText", "BrewingArrow", "BrewingBubbles", "BrewingPlateBar" };
+char* _StringFount16[] = { "" };//Brewing
+
+char* StringFount17[] = { "XVersion", "ControlsMultiList" };
+char* _StringFount17[] = { "aXsersion", "aControlsmultil" };
+
+char* StringFount18[] = { "XDynamicHtmlText", "HowToPlayContainer_13", "iggy_EnchantmentBook", "iggy_slot_Button_1", "iggy_slot_Button_2", "iggy_slot_Button_3", "label_1_10", "label_2_10", "label_3_10", "label_4_10", "ect" };
+char* _StringFount18[] = { "Label128", "Label228" };
+
+char* StringsFount19[] = { "XHowToList" };
+char* _StringsFount19[] = { "aXhowtolist" };
+
+char* StringsFount20[] = { "XHowToList" };
+char* _StringsFount20[] = { "aXhowtolist_0" };
+
+char* StringsFount21[] = { "XMultiList", "Decription" };
+char* _StringsFount21[] = { "aXmultilist", "aDecription_0" };
+
+char* StringsFount22[] = { "XButton1", "Button2", "Button3", "Button4", "Button5", "Button6", "Button7", "ButtonUpsell" };
+char* _StringsFount22[] = { "aXbutton_1", "aButton2_0", "aButton3_0", "aButton4", "aButton5", "aButton6", "aButton7", "aButtonupsell" };
+
+char* StringsFount23[] = { "XMainPanel", "BitmapBiomeMap", "NavigatorBiomeMap", "PlayableAreaLabel", "FullMapLabel", "CoordinatesLabel", "Timer", "Html_Help", "cursor", "ScrollUp", "ScrollDown", "ScrollLeft", "ScrollRight" };
+char* _StringsFount23[] = { "aXmainpanel", "aBitmapbiomemap", "aNavigatorbiomemap", "aPlaybleareala", "aFullmaplabel", "aCoordinateslab", "aTimer_1", "aHtmlhelp", "aCursor", "aScrollup", "aScrolldown", "aScrollleft", "aScrollRight" };
+
+char* StringsFount24[] = { "Particle Tick" };
+char* _StringsFount24[] = { "aParticleTick" };
+
+char* StringsFount25[] = { "Level tick" };
+char* _StringsFount25[] = { "aLevelTick" };
+
+char* StringsFount26[] = { "Level entity tick" };
+char* _StringsFount26[] = { "aLevelEntityTic" };
+
+char* StringsFount27[] = { "Level renderer tick" };
+char* _StringsFount27[] = { "aLevelRendererT" };
+
+char* StringsFount28[] = { "Game renderer tick" };
+char* _StringsFount28[] = { "aGameRendererT" };
+
+char* StringsFount29[] = { "LivingEntityRenderer %d" };
+char* _StringsFount29[] = { "aLivingentityre" };
+
+char* StringsFount30[] = { "Texture tick" };
+char* _StringsFount30[] = { "aTexturetick" };
+
+char* StringsFount31[] = { "Light Update" };
+char* _StringsFount31[] = { "aLightUpdate" };
+
+char* StringsFount32[] = { "Region::setCachedBlocksAndData" };
+char* _StringsFount32[] = { "aRegionSetcache" };
+
+char* StringsFount33[] = { "Setup", "Getting area", "Copying values", "Copying cache", "Copying cached block" };
+char* _StringsFount33[] = { "aSetup", "aGettingArea", "aCopyingValues", "aCopyingCache", "aCopyingCachedB" };
+
+char* StringsFount34[] = { "Entity tick", "Check for fire", "Play fizz sound", "Get cubes", "Clipy", "ClipX", "ClipZ", "Step up", "Falling check", "Check fall damage", "Make step sound", "checkInsideBlocks", "Update entity after falling" };
+char* _StringsFount34[] = { "aEntityTick", "aCheckForFire", "aPlayFizzSound", "aGetCubes", "aClipy", "aClipx", "aClipz", "aStepUp", "aFallingCheck", "aCheckFallDamag", "aMakeStepSound", "aCheckinsideblo", "aUpdateEntityAf" };
+
+char* StringsFount35[] = { "Step up", "Falling check", "Check fall damage" };
+char* _StringsFount35[] = { "aStepUp", "aFallingCheck", "aCheckFallDamag" };
+
+char* StringsFount36[] = { "Monster tick" };
+char* _StringsFount36[] = { "aMonsterTick" };
+
+char* StringsFount37[] = { "DLCDrive", "Input manager tick", "Profile manager tick", "Storage manager tick", "Render manager tick", "Sentient tick", "Network manager do work #1", "Frame present", "Profile load check", "Network manager do work #2" };
+char* _StringsFount37[] = { "aDlcdrive", "aInputManagerTi", "aProfileManager", "aStorageManager", "aRenderManagerT", "aSentientTick", "aNetworkManager", "aFramePresent", "aProfileLoadChe" };
+
+namespace Offsets
+{
+	enum Address
+	{
+		//Offset: 013621DC = 014C5AA0 = 00CE3C0C
+		//Main: 01412B30 = 014C5AA0 = 00CE3C0C
+		//Im guessing Entity address: 0x0151DD70
+		LOCAL_PLAYER = 0x14DF564,
+		STRINGS_FOUNT = 0x00F7823C,
+		XP_SHOW = 0x00AE9E40,
+		Block_Break_Particles = 0x00B49FC8,
+		Particles_Size = 0x00B49D64,
+		Particles_Amount = 0x00B49D74,
+		Player_Feet_Movement = 0x0040C1F8,
+		Feet_Angle = 0x013112AC,
+		Particles_Height = 0x00517C08,
+		LOCAL_PICK_UP = 0x0151EF04,
+		PICKED_UP_ITEM = 0x014C9250,
+		ITEM_REMOVED = 0x002FB4AC,
+		EMPTY_INVENTOY = 0x002FB484,
+		something = 0x1412B30,
+		freeze_Particles = 0x00AE9EF0,//00AF0F88
+		INSTANT_REFRESH_HUD = 0x00967FD8,
+		LOADING_BOX_LOCAL = 0x014CE734,
+		SOME_POINTERS = 0x004E0ADC,
+		MIGHT_BE_SOMETHING = 0x00BC53B8,
+		SOME_NATIVE = 0x016BD9E8,
+		TIP_STRING = 0x014017EA,
+		TITLE_STRING = 0x014017E4,
+		PROGRESSBAR_STRING = 0x14017D8,
+		TITLE_0_STRING = 0x14018F1,
+		XRESPOND_STRING = 0x014018DF,
+		RENDER_PLAYER = 0x00AEB968,
+		LOCAL_PLAYER_RENDER_ID = 0x016BD8C8,
+		PLAYER_POSITION_FLOAT = 0x0130F6F8,
+		PLAYER_SQUAT_TOGGLE = 0x004AE62C,
+		CONNECTION_TO_SERVER_TOGGLE = 0x00492810, //Anti-kick
+		SLEEPING_STRING = 0x000AEBB38,
+		MINING_SOUNDS = 0x00AE67AC,
+		PLAYER_HOLDING_ITEM_AMOUNT = 0x0151E6E8,
+		LOCAL_LAPIS = 0x014024D8,
+		OTHER_STRINGS = 0x00F78630,
+		OTHER_STRINGS_2 = 0x00F789B4,
+		OTHER_STRINGS_3 = 0x00F7941C,
+		OTHER_STRINGS_4 = 0x00F79594,
+		OTHER_STRINGS_5 = 0x00F797B8,
+		OTHER_STRINGS_6 = 0x00F77C00,
+		OTHER_STRINGS_7 = 0x00F76F7C,
+		OTHER_STRINGS_8 = 0x00F76A78,
+		OTHER_STRINGS_9 = 0x00F763D8,
+		OTHER_STRINGS_10 = 0x00F76088,
+		OTHER_STRINGS_11 = 0x00F758D8,
+		OTHER_STRINGS_12 = 0x00F753EC,
+		OTHER_STRINGS_13 = 0x00F74DDC,
+		OTHER_STRINGS_14 = 0x00F7493C,
+		OTHER_STRINGS_15 = 0x00F746C0,
+		OTHER_STRINGS_16 = 0x00F742D8,
+		OTHER_STRINGS_17 = 0x00F73CC4,
+		OTHER_STRINGS_18 = 0x00F738AC,
+		OTHER_STRINGS_19 = 0x00F72D94,
+		OTHER_STRINGS_20 = 0x00F72C1C,
+		OTHER_STRINGS_21 = 0x00F72AE8,
+		OTHER_STRINGS_22 = 0x00F7279C,
+		OTHER_STRINGS_23 = 0x00F721B0,
+		OTHER_STRINGS_24 = 0x00AE9EFC,
+		OTHER_STRINGS_25 = 0x00AE9E8C,
+		OTHER_STRINGS_26 = 0x00AE9C50,
+		OTHER_STRINGS_27 = 0x00AE9BD8,
+		OTHER_STRINGS_28 = 0x00AE9AD8,
+		OTHER_STRINGS_29 = 0x00AE8E4C,
+		OTHER_STRINGS_30 = 0x00AE7810,
+		OTHER_STRINGS_31 = 0x00AEB898,
+		OTHER_STRINGS_32 = 0x0051A028,
+		OTHER_STRINGS_33 = 0x000F07D0,
+		OTHER_STRINGS_34 = 0x00229984,
+		OTHER_STRINGS_35 = 0x0022B6D8,
+		OTHER_STRINGS_36 = 0x0045FBE0,
+		OTHER_STRINGS_37 = 0x00962264,
+		//000012D60
+		DISABLE_FOOTSTEP_SOUND = 0x0022C060,
+		LOCAL_FOOTSTEP_SOUND = 0x14C9994,
+		PLAYER_FEET_MOVEMENT_RUNNING = 0x0040C1F8,
+		LOCAL_BODY_ANGLE_FLOAT = 0x13112AC,
+		SHOW_CRAFTING_BOX = 0x00782140,
+		LOCAL_CRAFTING_BOX = 0x132AE78,
+		MIX_GAME_TEXT = 0x00EC3ED8,
+		PRINT_TEXT = 0x00A6F270,
+		PRINT_TEXT_2 = 0x00A6F298,
+		DISABLE_PLACE_BLOCK = 0x00AE4A94, //00CEC7B8
+		DISABLE_SWIMMING = 0x00AF9ED8,
+		GAME_MODE_CHANGED_TEXT = 0x01511684,
+		DISABLE_SWIMMING_2 = 0x00AFA170,
+		PLAYER_BODY_LOCAL = 0x014C9994,
+		BODY_STAYS_IN_SWIMMING_POSITION = 0x00AFA288,
+		DISABLE_RUN_ON_GROUND = 0x00AFA2CC,
+		GAMEPLAY_SPEED = 0x00613940,
+		PLAYER_RUN_SPEED_FLOAT = 0x00AF9718,
+		PLAYER_BODY_ANGLE = 0x013112AC,
+		LOCAL_MONSTERS = 0x0151DD78,
+		LOCAL_CREEPER_EXPLODE = 0x014C9138,
+		LOCAL_CREEPER_GROW = 0x014C95D4,
+		LOCAL_FIRE = 0x0137A200, //Not fully sure
+		LOCAL_PLAYER_ID = 0x014DF564,
+		SAVING_BOX_STAYS = 0x00865DC4,
+		ENABLE_RAIN_BOOL = 0x0038F36C,
+		ENABLE_RAIN_IF_CLEAR = 0x0038F3D4,
+		LOCAL_RAIN = 0x0155A780,
+		KEEP_INVENTORY = 0x002AC2D0,
+		HIDE_HUD = 0x00874400,
+		HOST_OPTIONS = 0x00952BB0,
+		MAKE_IT_RAIN_2 = 0x0038F73C,
+		REGENERATE_HEATH_2 = 0x002AC438, //68 63 00 00 to 68 63 00 01
+		SHOW_PREVIOUS_ITEM_IN_BLANK_SLOT = 0x00300430,
+		SHOW_ITEMS_IN_HAND = 0x00300474,
+		LOCAL_ITEM_IN_PLAYER_HAND = 0x014C9250,
+		BUTTONS_PRESSED_ADDRESS = 0x016CD9E8, //1.82
+	};
+	enum  Pointers
+	{
+		POINTER_ONE = 0x135F499C,
+		POINTER_TWO = 0xFFFFFFFF88233440,
+		POINTER_THREE = 0x2B9DE1E9,
+		POINTER_FOUR = 0x569CC9E7,
+		POINTER_FIVE = 0x552A6DC4,
+		POINTER_SIX = 0x88ACB12,
+		POINTER_SEVEN = 0xFFFFFFFFBEB2B8C4,
+	};
+
+	enum Entity
+	{
+		//Start of Entity Class is dword_13959E8 for 1.82
+		setStatusFlag,
+	};
+};
+/*
+//ENDERDRAGON_STRING:.long 0x139DF70
+//MOB_STRING:     .long 0x13BF7E8
+//MINECART_STRING:.long 0x13BF7F0
+//MINECARTTNT_STRING:.long 0x13BF800
+//BONEMEALBLOCK_STRING:.long 0x1376F58
+//TRANSPARENTBLOCK_STRING:.long 0x13EBBD8
+//STRUCTURESTART_STRING:.long 0x13EBBC0
+//TRAPDORRBLOCK_STRING:.long 0x13EBC28
+//BEDBLOCKENTITY_STRING:.long 0x1377770
+//BLOCKENTITY:    .long 0x137F6E8
+//PLAYER_STRING:  .long 0x13CADC0
+//TEAM_STRING:    .long 0x13EB958
+//MONSTER_STRING: .long 0x13BFFB0
+//ABSTRACT_SKELETON_STRING:.long 0x1376FD8
+//ABSTRACT_HORSE_STRING:.long 0x1376AA0
+//ANIMALSTRING:   .long 0x1375F98
+//ZOMBIE_STRING:  .long 0x13F7360 
+//VINDICATOR_STRING:.long 0x13F7410
+//PLAYER PED NOT THERE: 0040F8C8
+//BATMODEL_STRING:.long 0x141B058
+//ENTITYRENDERER_STRING:.long 0x141D7F0
+//BREAKINGITEMPARTICLE_STRING:.long 0x141B1F0
+//PARTICLE_STRING:.long 0x14276E8
+//MOBRENDERER_STRING:.long 0x1424418
+//LIVINGENTITY_STRING:.long 0x14241F0
+//LOCAL_PLAYER_STRING:.long 0x1424220
+//ABSTRACTCLIENTPLAYER_STRING:.long 0x1418610
+//MULTIPLAYERLOCALPLAYER_STRING:.long 0x1424230
+//LOCAL_PLAYER_STRING_POINTER_2:.long 0x1424220
+//1.82 Eboot TOC: 0014A14E0
+//Local Monsters 1.82 0152EAD0
+//sub_D0F20C Something
+//Might be Air Attack speed for 1.82 0x01530014
+//getMultiplayerLocalPlayerBitThing for 1.82: 0x0022CEE0
+//damageServerPlayer for 1.82: 004A94C0
+//getServerPlayerInvisibility for 1.82: 0049E5B0
+//setServerPlayerHealth for 1.82: 004B1F64
+//calledWhenGetDamaged for 1.82: 004B1D88
+//checkBit9OnMultiplayerLocalPlayer for 1.82: 00234964
+//globalVariablesStruct is LOCAL PLAYER ADDRESS
+//ServerPlayeris4_r3_Sometimes_1 for 1.82: 0039DA88
+//ServerPlayeris4(r3)Sometimes for 1.82: 00229408
+//importantnServerPlayerIs0(R6) for 1.82: 0095138
+//importantnServerPlayerIsR3_4 for 1.82: 004AE698
+//importantnServerPlayerIsR3_3 for 1.82: 003A76C8
+//FireWorks Size: 01322F34
+//Maybe StepSounds: 0x014D8CC0
+//Disable Step Sounds: 00227C50
+//Entity stick to ground: 00228024
+//PED CANT MOVE: 00D436EC
+//PED_MOVEMENT_SPEED: 0003ABD50
+//ServerPlayer TOC: 001333C60
+//All Mobs and peds spawn up more: 00389B64
+//Breakpoint Function 1.82: 00A75CCC
+//particles 1.82: 00B54508
+//PARTICLE_STRING:.long 0x14276E8
+//Something 0x0070B738 Pattern List: 0013FD338
+//Create Schematic String: 013FBF40
+//HowToPlay String: 013FB9FC
+//DebugMenu Table: 013FB8CC
+//0x14351C0         # St7codecvtIccSt9_MbstatetE = Encapsulates conversion of character strings, including wide and multibyte from one encoding to another
+//0x1377A48         # St12codecvt_base = Provides the conversion status constants which are inherited and used by the std::codecvt facets.
+//0x1435268         # St13basic_filebufIwSt11char_traitsIwEE = Whose associated character sequence is a file
+//0013959F0
+//0014A0D58
+//0137672C
+Disable Firework Launch Sound: 0028FFE4
+Fireworks Dont Launch: 0028FB3C
+Firworks Launch Height: 0028FA60
+Firework Launcher visible: 0x0028EDE0
+Firework dont explode: 0x0028F090
+Fireworks left player: 002255F8
+Fireworks Offset: 0014D9270
+Doing Something with all players: 152F004
+Server Tick Location: 00B4200C
+ITEM_INSTANCE = 0x0152EAD4
+BYTE_ENTITY = 0x0152EAC4
+INT_ENTITY = 0x0152EAC8
+FLOAT_ENTITY = 0x0152EACC
+STRING_ENTITY = 0x0152EAD0
+BLOCK_STATE = 0x0152EAD8
+BOOLEAN_ENTITY = 0x0152EADC
+ROTATIONS_ENTITY = 0x0152EAE0
+BLOCK_POS_ENTITY = 0x0152EAE4
+OPTIONAL_BLOCK_POS_ENTITY = 0x0152EAE8
+DIRECTION_ENTITY = 0x0152EAEC
+COMPOUND_TAG_ENTITY = 0x0152EAF0
+SEEDFOOD_ITEM = 0x01561530
+SEED_FOOD_ITEM = 0x01561534
+FOOD_ITEM = 0x01561538
+CARROTONASTICK_ITEM = 0x01561548
+SEAFOOD_ITEM = 0x01561208
+DyeColor = 0x0152DFD8
+PLAYER MINING DISTANCE: 0x1322F30
+DAMAGE SOURCE: 0x01521B64
+INDIRECTENTITYDAMAGESOURCE: 0x01521B68
+ENTITYDAMAGESOURCE: 0x01521B6C
+CREATIVE MODE IN SURVIVAL: 002F0350
+CREATIVE MODE IN SURVIVAL 2: 0x00AAF6AC
+DISABLE MINING: 000AED8BC
+MOB CALLER: 0013BEF70
+MOB STRING: 013BEF74
+MINECART STRING: 013BEF84
+MONSTER STRING: 013BF304\
+ZOMBIE_STRING: 0x13F3C3C
+ANIMAL_STRING: 0x1375F9C
+ARROW STRING: 0137623C
+ABSTRACT HORSE STRING: 0136F89C
+ABSTRACT SKELETON STRING: 0137624C
+PLAYER STRING: 0x013C4704
+LIVING ENTITY STRING: 0x013B0434
+ITEM ENTITY: 0x013AA3C4
+HOPPERBLOCK ENTITY STRING: 0x013AA3A4
+NAMEABLE STRING: 01375D2C
+CONTAINER OFFSET: 0x0013AA334
+HOPPER STRING: 0x0013AD01C
+TITLE: 0x15632E0
+AUTHOR: 0x15632FC
+COW STRING: 0x00138ACFC
+PIG STRING: 0013C9DF4
+MOBS TARGET HOST: 007F4770 - 00 00 00 00
+DAMAGE_SOURCE: 01521B44
+NO DROWNING: 0X00003A7BBC
+Item Property: 0x0152F0C8
+ITEM INFO FUNCTION: 0031F498 - 0031F894
+SOMETHING WITH GHOST PLAYER: 0x0080F3CC
+*/
+
+/* ENTITYS::
+DOLPHIN: 152ECA4, DOLPHIN STRING: 01393E5C, DOLPHIN ENTITY: 01393E58
+HORSE::TRAVEL STRING: 01310720
+ENDERDRAGON STRING ADDRESS: 01396904, 
+Remove Hud when saving; 000884114
+End Crystal: 01396800 - 0026C3BC
+ITEMPROPERTYFUNCTION_COOLDOWN: 0152F0CC
+Phantom Membrane: 0x131F75C
+Fireworks Item: 0x139CB00
+FireworkChargeItem: 0x139CA30
+Might be EnderPearl: 152F004
+Dye color for book maybe: 0x0152DFE4
+AirItem: 14D9328
+HAPPY VILLAGER STRING: 1560CD8
+EyeOfEnderSignal: 0x1398D58
+BOOLEAN_PROPERTY: 0x152E8DC
+DOORBLOCK: 0x152DF7C
+ENCHANTMENT BOTTLE: 0x014D928C
+ELYSTRA CAN FLY: 0x000301BC0
+ITEMPROPERTYFUNCTION_DAMAGED: 0x152F0C0
+EVENTIMPL: 0x1567C58
+ITEM IN HAND DELETE: 0x00030CD74
+EGG DYECOLOR: 0x152DFE4
+POTION ITEM: 0x014D954C
+IntegerProperty: 0x15624A4
+DefaultedMappedRegistry: 0x14FD610
+StandingSignBlock: 0x014D8CA4
+WALLSIGNBLOCK: 0x014D8CCC
+Web Block: 0x14D8C20
+HtmlText_WithPanel offset: 13FFDE8
+GOLDEN APPLE TURNS ENCHANTED: 0x002BEA84
+Attack Damage Effect: 0x014D9DA4
+Disable Poison: 0x003A30EC
+LOCAL PLAYER: 0x014DF564 - 0141BC28 - 
+REMOTE PLAYER MODEL: 0x014D9178 - Same as 00222AB0
+PROFILE: 16CD9F8 - 0142BB78
+WHAT PLAYER IS DOING: 1711E04 - 0x01711F50
+PLAYER USER: 3000AB80
+*/
+
+/* POTIONS ADDRESSES
+SPEED_OFFSET: 0x14D9D94
+SNOWNESS_OFFSET: 0x14D9D98
+HASTE_OFFSET: 0x14D9D9C
+MINING_FATIGUE: 0x14D9DA0
+STRENGTH_OFFSET: 0x14D9DA4
+INSTANT_HEALTH OFFSET: 0x14D9DA8
+INSTANT_DAMAGE OFFSET: 0x14D9DAC
+JUMP_BOOST OFFSET: 0x14D9DB0
+NAUSEA OFFSET: 0x14D9DB4
+REGENERATION OFFSET: 0x14D9DB8
+RESISTANCE OFFSET: 0x14D9DBC
+FIRE_RESISTANCE OFFSET: 0x14D9DC0
+WATER_BREATHER OFFSET: 0x14D9DC4
+INVISIBILITY OFFSET: 0x14D9DC8
+BLINDNESS OFFSET: 0x14D9DCC
+NIGHT_VISION OFFSET: 0x14D9DD0
+HUNGER OFFSET: 0x14D9DD4
+WEAKNESS OFFSET: 0x14D9DD8
+POISON OFFSET: 0x14D9DDC
+WITHER OFFSET: 0x14D9DE0
+HEALTH_BOOST OFFSET: 0x14D9DE4
+ABSORPOTION OFFSET: 0x14D9DE8
+SATURATION OFFSET: 0x14D9DEC
+GLOWING OFFSET: 0x14D9DF0
+LEVITATION OFFSET: 0x14D9DF4
+LUCK OFFSET: 0x14D9DF8
+UNLUCK OFFSET: 0x14D9DFC
+CONDUIT_POWER OFFSET: 0x14D9E00
+SLOW_FALLING OFFSET: 0x14D9E04
+
+*/
+
+
+void sub_AAF824(MultiPlayerGameMode *, BlockPos);
+void sub_AB06B8(MultiPlayerGameMode, BlockPos BlockItem);
+//off_14DF564 Maybe Block Position
+
+void SetStatusFlag()
+{
+	setStatusFlag(int, int, bool);
+}
+
+opd_s Block_Break_Particles_FX_s = { Offsets::Block_Break_Particles, TOC };
+void Particles()
+{
+	*(float *)(0x00517C08) = 1.01712e+09;
+}
+void PLAYER_ID()
+{
+	int LOCAL_PLAYER[2] = { Offsets::LOCAL_PLAYER_ID, TOC };
+	typedef int(*pLocalPlayer)();
+	pLocalPlayer PLAYER_PED_ID = (pLocalPlayer)LOCAL_PLAYER;
+}
+void ENTITY_INVISIBLE(int entity, bool toggle)
+{
+	int SetEntityInvisible[2] = { 0x2346E8, TOC };
+	typedef int(*pSetEntityInvisibleFn)(int entity, bool toggle);
+	pSetEntityInvisibleFn SetEntityInvisible = (pSetEntityInvisibleFn)SetEntityInvisible;
+}
+
+void SetPlayerInvisible()
+{
+	ENTITY_INVISIBLE(PLAYER_ID(), true);
+}
+
+
+void(*Block_Break_Particles_FX) (float x, float y, float width, float height, float xScale, float yScale, float xay, float yay) = (void(*) (float x, float y, float  width, float height, float xScale, float yScale, float xay, float yay))&Block_Break_Particles_FX_s;
+
+
+void DrawParticles(float x, float y, float width, float height)
+{
+	Block_Break_Particles_FX(x, y, width, height, 1, 1, 1, 1);
+}
+
